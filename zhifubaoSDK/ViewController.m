50918cbc6ce7420a3fa15618dd58723d1ae9b3e4
frank <fk911c@gmail.com>
2015-01-13 17:39:56 +0800

    Initial Commit

---
+++ zhifubaoSDK/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zhifubaoSDK
+//
+//  Created by cen on 13/1/15.
+//  Copyright (c) 2015 cen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ zhifubaoSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhifubaoSDK
+//
+//  Created by cen on 13/1/15.
+//  Copyright (c) 2015 cen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
